e4185595655d43caa4643411695cb6b1c91417fd
zhaodajiang <1977863262@qq.com>
2020-01-02 20:23:19 +0800

    v2.0

--- src/main/java/com/baizhi/Application.java
+++ src/main/java/com/baizhi/Application.java
@@ -10,7 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.pringln("赵大江大帅逼！！");
     }
 
 }
